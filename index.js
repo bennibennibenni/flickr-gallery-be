a850e30737761ad5a3bd8c06ece9b593917122b2
DESKTOP-H6T6TEO\benni <bennixie98@gmail.com>
2021-10-09 20:40:28 +0700

    feat: Add endpoint for fetch flickr public image :sparkles:

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Start is running.....");
+});
+
+app.get("/images", async (req, res) => {
+  let { tags, pageSize = 4, currentPage = 1 } = req.query;
+  currentPage = +currentPage;
+  pageSize = +pageSize;
+  const startAt = currentPage === 1 ? 0 : pageSize * (currentPage - 1);
+  try {
+    const response = await axios(
+      `https://www.flickr.com/services/feeds/photos_public.gne?format=json&${
+        tags && `&tags=${tags}`
+      }`
+    );
+    let { data } = response;
+    data = data.replace("jsonFlickrFeed(", "");
+    data = data.substring(0, data.length - 1);
+    data = JSON.parse(data);
+    const images = data.items.slice(startAt, startAt + pageSize);
+    const totalData = data.items.length;
+    const totalPage =
+      ~~(totalData / pageSize) === 0 ? 1 : ~~(totalData / pageSize);
+    const { modified } = data;
+    res.json({
+      data: images,
+      meta: {
+        currentPage,
+        totalPage,
+        pageSize,
+        totalData,
+        modified,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
