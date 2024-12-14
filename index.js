3a5fe7c90083ecd30c18c0ff12246e40e6e66dd5
Benni <bennixie98@gmail.com>
2024-12-14 12:57:46 +0700

    update flickr api url

--- index.js
+++ index.js
@@ -12,42 +12,45 @@ app.get("/", (req, res) => {
   res.send("Start is running.....");
 });
 
-app.get("/images", async (req, res) => {
+app.get("/images", (req, res) => {
   let { tags, pageSize = 4, currentPage = 1 } = req.query;
   currentPage = +currentPage;
   pageSize = +pageSize;
+  let url = tags
+    ? `https://api.flickr.com/services/feeds/photos_public.gne?tags=${tags}&tagmode=any&format=json`
+    : "https://api.flickr.com/services/feeds/photos_public.gne?format=json";
   const startAt = currentPage === 1 ? 0 : pageSize * (currentPage - 1);
-  try {
-    const response = await axios(
-      `https://www.flickr.com/services/feeds/photos_public.gne?id=76499814&format=json&${
-        tags && `&tags=${tags}`
-      }`
-    );
- 
-    console.log(response)
-    let { data } = response;
-    data = data.replace("jsonFlickrFeed(", "");
-    data = data.substring(0, data.length - 1);
-    data = JSON.parse(data);
-    const images = data.items.slice(startAt, startAt + pageSize);
-    const totalData = data.items.length;
-    const totalPage =
-      ~~(totalData / pageSize) === 0 ? 1 : ~~(totalData / pageSize);
-    const { modified } = data;
-    res.json({
-      data: images,
-      meta: {
-        currentPage,
-        totalPage,
-        pageSize,
-        totalData,
-        modified,
+  axios
+    .get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
       },
+    })
+    .then((response) => {
+      let { data } = response;
+      data = data.replace("jsonFlickrFeed(", "");
+      data = data.substring(0, data.length - 1);
+      data = JSON.parse(data);
+      const images = data.items.slice(startAt, startAt + pageSize);
+      const totalData = data.items.length;
+      const totalPage =
+        ~~(totalData / pageSize) === 0 ? 1 : ~~(totalData / pageSize);
+      const { modified } = data;
+      res.json({
+        data: images,
+        meta: {
+          currentPage,
+          totalPage,
+          pageSize,
+          totalData,
+          modified,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).send("Error fetching data");
     });
-  } catch (error) {
-    console.log(error.message);
-    res.json({ error });
-  }
 });
 
 app.listen(PORT, () => {
